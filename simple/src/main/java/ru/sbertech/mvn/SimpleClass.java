514acf766f345236de35d7515b3fee2811c89e80
AntonSaburov <AntonSaburov@yandex.ru>
2016-10-17 22:00:25 +0300

    Maven - first step

---
+++ simple/src/main/java/ru/sbertech/mvn/SimpleClass.java
@@ -0,0 +1,26 @@
+package ru.sbertech.mvn;
+
+/**
+ * Created by anton on 10/17/16.
+ */
+public class SimpleClass
+{
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ simple2/src/main/java/ru/sbertech/mvn2/SimpleClass123.java
@@ -0,0 +1,13 @@
+package ru.sbertech.mvn2;
+
+import ru.sbertech.mvn.SimpleClass;
+
+/**
+ * Created by anton on 10/17/16.
+ */
+public class SimpleClass123
+{
+    public static void main(String[] args) {
+        SimpleClass sc = new SimpleClass();
+    }
+}
